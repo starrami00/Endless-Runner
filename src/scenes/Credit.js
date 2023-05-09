d2113042f3f92a79588ad56134e7e2ca03e0518d
starrami00 <starrami@ucsc.edu>
2023-05-08 22:53:10 -0700

    Adding background music and credit scene

--- src/scenes/Credit.js
+++ src/scenes/Credit.js
@@ -1,5 +1,28 @@
-class Menu extends Phaser.Scene {
+class Credit extends Phaser.Scene {
     constructor() {
         super("creditScene");
     }
+    preload() {
+        // load credit screen
+        this.load.image('CreditScreen', './assets/creditScreen.png');
+        // load audio
+        this.load.audio('bg_music', './assets/titleBgMusic.wav');
+    }
+
+    create() {
+        // credit config
+        let title = this.add.sprite(0,0,'CreditScreen');
+        title.setOrigin(0,0);
+        
+        keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
+    }
+
+    update() {
+        if(Phaser.Input.Keyboard.JustDown(keyLEFT)) {
+            // Go to Menu
+            this.scene.start('menuScene');
+        }
+    
+    }
+    
 }
